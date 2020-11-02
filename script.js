1f74d642f910f5694eac0acc6b81e081bd6b1275
Bobbi <soundwebvertising@gmail.com>
2020-11-01 20:37:35 -0800

    Got columns and rows working

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 function myFunction() {
   let x = 16;
-  for (var rows = 0; rows < x; rows++) {
-    var theDiv = document.getElementById("container");
-    let myDiv = document.createElement("DIV");
-    myDiv.className = "grid-item";
-    theDiv.appendChild(myDiv);
-
+  for (var rows = 0; rows < x; rows++) {  
+    for (var columns = 0; columns < x; columns++) {
+      let theDiv = document.getElementById("container");
+      let myDiv = document.createElement("DIV");
+      myDiv.className = "grid-item";
+      theDiv.appendChild(myDiv);
+    }
   }
   console.log("running my function");
 }         // Append <div grid item> to <div> with id="container"
 
 
 
-myFunction();
+//myFunction();
