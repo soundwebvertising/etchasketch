655843deda1878844214d19d88782d2317e3001e
Bobbi <soundwebvertising@gmail.com>
2020-11-11 09:14:11 -0800

    added JS code to change background color on mouseover event.

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
+let gridNum = 0;
+let theDiv;
+let myNodes;
+let mybkground;
+let gridItems;
+
 function myFunction() {
   let x = 16;
   for (var rows = 0; rows < x; rows++) {  
     for (var columns = 0; columns < x; columns++) {
+      gridNum++;
       let theDiv = document.getElementById("container");
       let myDiv = document.createElement("DIV");
       myDiv.className = "grid-item";
       theDiv.appendChild(myDiv);
+      gridItems = document.querySelectorAll('.grid-item')
     }
   }
-  console.log("running my function");
-}         // Append <div grid item> to <div> with id="container"
+  gridItems.forEach(gridItem => {
+    gridItem.addEventListener('mouseover', () => {
+      console.log(gridItem);
+      gridItem.classList.add('img-darken')
+    })
+  });
+}    
 
 
-
-//myFunction();
